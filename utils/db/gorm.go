93095b38249c214b3ec6a8943e80e6da14f671b6
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-01 18:49:48 +0700

    feat: add mysql gorm connection

--- utils/db/gorm.go
+++ utils/db/gorm.go
@@ -1,1 +1,15 @@
 package db
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+)
+
+func GormMysql() *gorm.DB {
+	db, err := gorm.Open(mysql.Open("root:1234QWERasdf.@tcp(localhost:3306)/gorm-backend?charset=utf8mb4&parseTime=True&loc=UTC"), &gorm.Config{})
+	if err != nil {
+		log.Println("gorm.open", err)
+	}
+	return db
+}
