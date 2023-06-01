6afb672e1a134c5221f1f61dd99d97ca20a157b7
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-01 18:53:11 +0700

    feat: add default error response with message

--- dto/response-error.go
+++ dto/response-error.go
@@ -5,3 +5,15 @@ type ErrorResponse struct {
 	Data   any `json:"data"`
 	Errors any `json:"errors,omitempty"`
 }
+
+func DefaultErrorResponseWithMessage(msg string) ErrorResponse {
+	return ErrorResponse{
+		ResponseMeta: ResponseMeta{
+			Success:      false,
+			MessageTitle: "Oops, something went wrong.",
+			Message:      msg,
+			ResponseTime: "",
+		},
+		Data: nil,
+	}
+}
